fe5d69e17873707774dd6da4656f35aecd354794
wilgnerjr <76569878+wilgnerjr@users.noreply.github.com>
2023-01-22 02:57:10 -0400

    .

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('.container'),
+qrInput = container.querySelector('.form input'),
+generateBtn = container.querySelector('.form button'),
+qrImg = container.querySelector('.qr-code .img');
+
+
+generateBtn.addEventListener('click', () => {
+    let qrValue = qrInput.value;
+    if(!qrValue){
+        alert('Insira uma URL ou um texto para gerar o codigo QR')
+        return;
+    }
+    generateBtn.innerHTML = 'Gerando um QR code';
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=%24${qrValue}`;
+})
